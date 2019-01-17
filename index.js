6cd4ecaae89626de8c91b4632eda8882d0a96781
Sarah Bennedsen <sarahbennedsen@Sarahs-MacBook.local>
2019-01-16 22:10:45 -0500

    more stuff

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+const compression = require('compression');
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 express()
+	.set('x-powered-by', false)
+	.use(compression())
   .use(express.static(path.join(__dirname, 'public')))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
