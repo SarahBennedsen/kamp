28bc8dc4809358ab4c4171e3c02745673603fe6f
Sarah Bennedsen <sarahbennedsen@Sarahs-MacBook.local>
2019-02-10 18:53:37 -0500

    ssl redirect

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const compression = require('compression');
 const express = require('express');
 const path = require('path');
+const requireHttps = require('./require-https')
 const PORT = process.env.PORT || 5000;
 
 express()
 	.set('x-powered-by', false)
 	.use(compression())
+	.set('trust proxy', true)
+	.use(requireHttps)
   .use(express.static(path.join(__dirname, 'public')))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
